a71c3bdf1c5baa759bb3e70c2a55d3c7b604a6dc
MichaelAbe <michael.k.abe@gmail.com>
2020-12-08 20:18:24 -0500

    commit tree 03501e00619a7214babf1e5d633d9d3241f2851d

---
+++ lib/create.sql
@@ -0,0 +1,26 @@
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding_goal INTEGER,
+    start_date TEXT,
+    end_date TEXT
+);
+
+--projects (id, title, category, funding_goal, start_date, end_date) 
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT, 
+    age INTEGER
+);
+-- users (id, name, age)
+
+CREATE TABLE pledges (
+    id INTEGER PRIMARY KEY,
+    amount INTEGER, 
+    user_id INTEGER,
+    project_id INTEGER
+);
+
+--pledges (id, amount, user_id, project_id) 
